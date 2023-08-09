36fb17c58ebf5e584360e391864b33782fff948e
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2023-08-09 21:08:34 +0700

    adjust select field

--- call_center_s1/src/App.js
+++ call_center_s1/src/App.js
@@ -2,18 +2,22 @@ import './App.css';
 import InputInformation from './feattures/InpuInformation';
 import ResponsiveAppBar from './components/header';
 import { Route, Routes, BrowserRouter} from 'react-router-dom'
-import connectDB from './utils/db.js'
+import React from 'react';
+
 import callAPI from './api/callAPI';
 import { useEffect } from 'react';
 
+
 function App() {
-  useEffect(()=>{
-    const fetchCalls = async () =>{
-      const calls= await  callAPI.getAll()
-      console.log(calls);
-    }
-    fetchCalls()
-  })
+  // useEffect(()=>{
+  //   const fetchCalls = async () =>{
+  //     const calls= await  callAPI.getAll()
+  //     console.log(calls);
+  //   }
+  //   fetchCalls()
+  // })
+  
+
   
   return (
     <BrowserRouter>
